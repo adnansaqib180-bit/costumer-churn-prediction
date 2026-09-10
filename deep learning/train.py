a7bc67874826b758906dc5c0203a53e4f4d41abd
adnansaqib180-bit <adnansaqib180@gmail.com>
2026-09-10 22:10:38 +0500

    Refactor model training script to improve hyperparameter tuning and evaluation metrics

--- deep learning/train.py
+++ deep learning/train.py
@@ -6,7 +6,8 @@ from keras.layers import Dense
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 # from imblearn.over_sampling import SMOTE 
-import keras_tuner as kt 
+import keras_tuner as kt
+
 
 df = pd.read_csv('costumer-churn-prediction/clasic machine learning/Data.csv')
 print(df.head())
@@ -32,6 +33,9 @@ for col in same_columns:
 x = df.drop(columns= ['Churn'])
 y = df['Churn']
 
+print(df.head())
+print(df.info())
+
 x,x_test,y_train,y_test = train_test_split(x,y,random_state=42)
 
 scaler =  StandardScaler()
@@ -45,19 +49,24 @@ x_test = scaler.transform(x_test)
 
 def build_model(hp):
     model = Sequential()
-    nodes = hp.Int('nodes',min_value= 20,max_value=128,steps= 8)
-    optimizer = hp.choice('optimizer',values=['Adam','sgd'])
+    nodes = hp.Int('nodes',min_value= 20,max_value=128,step= 8)
+    optimizer = hp.Choice('optimizer',values=['Adam','sgd'])
     activation = hp.Choice('activation',values=['tanh','sigmoid','relu'])
-    for i in range (hp.Int('layers',min_value=2,max_value=10)):
-        model.add(Dense(units=nodes,activation=activation))
-        model.compile(optimizer=optimizer,loss='binary_crossentropy',metrics=['accuracy'])
+    num_layers = hp.Int('layers',min_value=2,max_value=5)
+    for i in range (num_layers):
+        if i == 1:
+            model.add(Dense(units=nodes,activation=activation,input_shape=21))
+        else:
+            model.add(Dense(units=nodes,activation=activation))
+        model.add(Dense(1,activation='sigmoid'))          
+        model.compile(optimizer=optimizer,loss='binary_crossentropy',metrics=['f1_score'])
     return model
-tuner = kt.RandomSearch(build_model,objective='val_accuracy',max_trials=5)
+tuner = kt.RandomSearch(build_model,objective='val_f1_score',max_trials=5)
 tuner.search(x_train,y_train,epochs=10,validation_data=(x_test,y_test))
 print(tuner.get_best_hyperparameters()[0].values)
 model = tuner.get_best_models(num_models=1)[0]
 print(model.summary())
-
+history = model.fit(x_train,y_train,epochs=50,validation_split = .24)
 probabilities = model.predict(x_test)
 threshold = 0.45
 predictions = (probabilities > threshold).astype(int)
@@ -76,8 +85,8 @@ plt.ylabel("Loss")
 plt.xlabel("Epoch")
 plt.legend()
 plt.subplot(1, 2, 2)
-plt.plot(history.history["accuracy"], label="Train Accuracy")
-plt.plot(history.history["val_accuracy"], label="Validation Accuracy")
+plt.plot(history.history["f1_score"], label="Train Accuracy")
+plt.plot(history.history["val_f1_score"], label="Validation Accuracy")
 plt.title("Model Accuracy")
 plt.ylabel("Accuracy")
 plt.xlabel("Epoch")
@@ -88,5 +97,5 @@ plt.show()
 
 #saving the model
 
-model.save('trained_nural_network.keras')
-print('model saved')
+# model.save('trained_nural_network.keras')
+# print('model saved')
